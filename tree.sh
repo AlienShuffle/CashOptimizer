17c5f241b6ef2870f7c7d38f4b756b51965b00cc
AlienShuffle <readngtndude@gmail.com>
2024-11-08 10:12:19 -0500

    t

--- tree.sh
+++ tree.sh
@@ -4,17 +4,23 @@
 find * -type d -exec sh -c 'tree "$0" \
     -o "$0/index.html" \
     -H "https://cashoptimizer.pages.dev/$0" \
+    -T "$0" \
     -L 1 \
     --noreport \
-    --houtro "" \
     --dirsfirst \
     --charset utf-8 \
     --ignore-case \
     --timefmt "%d-%b-%Y %H:%M" \
-    -I "index.html" \
-    -T "$0" \
     -h -D \
+    -I "index.html" \
     -P "*.json|*.csv|*.html" \
     ' {} \;
-
-    # -L 1 - only on level deep
+# what is going on here?
+  # -o is location of the output file
+  # -H create HTML output using the parameter as path to tree.
+  #
+  # -I ignore list
+  # -P pattern match file inclusion list (not directories)
+# unused:
+  # -L 1 - only on level deep
+  # -houtro "" - not sure what this does!
