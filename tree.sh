837e46f140d85a367219859a400fe24bb98b0176
AlienShuffle <readngtndude@gmail.com>
2024-11-10 17:26:17 -0500

    comment out extra logging.

--- tree.sh
+++ tree.sh
@@ -6,7 +6,7 @@ if [ -d Banks ]; then
     cd Banks
     find * -depth -type d -print >/tmp/bank-list.txt
     if diff bank-list.txt /tmp/bank-list.txt; then
-      echo bank-list.txt unchanged.
+      #echo bank-list.txt unchanged.
     else
       cat /tmp/bank-list.txt >bank-list.txt
       echo bank-list.txt updated.
