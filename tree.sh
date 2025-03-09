c5a101577e779d1359fae7893fca69572ec10e2d
AlienShuffle <readngtndude@gmail.com>
2025-03-09 12:22:04 -0400

    cleanup tree

--- tree.sh
+++ tree.sh
@@ -5,16 +5,32 @@
 if [ -d Banks ]; then
   (
     cd Banks
-    find * -depth -type d -print >/tmp/bank-list.txt
+    find * -prune -type d -print >/tmp/bank-list.txt
     if diff bank-list.txt /tmp/bank-list.txt; then
       true
-      #echo bank-list.txt unchanged.
+      echo bank-list.txt unchanged.
     else
       cat /tmp/bank-list.txt >bank-list.txt
       echo bank-list.txt updated.
     fi
     rm /tmp/bank-list.txt
   )
+   for dir in Banks/*/history; do
+   (
+    echo "cd into $dir"
+    cd "$dir"
+    find * -prune -type d -print >/tmp/acct-list.txt
+    touch acct-list.txt
+    if diff acct-list.txt /tmp/acct-list.txt; then
+      true
+      echo "$dir/acct-list.txt unchanged."
+    else
+      cat /tmp/acct-list.txt >acct-list.txt
+      echo "$dir/acct-list.txt updated."
+    fi
+    #rm /tmp/acct-list.txt
+  )
+  done
 fi
 #
 # create the various index.html files.
