b4834e5589e694961a673893d095720d13962afc
AlienShuffle <readngtndude@gmail.com>
2025-03-10 12:37:52 -0400

    acct-list.txt touch cleanup.

--- tree.sh
+++ tree.sh
@@ -6,6 +6,7 @@ if [ -d Banks ]; then
   (
     cd Banks
     find * -prune -type d -print >/tmp/bank-list.txt
+    [ -f "bank-list.txt" ] || touch -list.txt
     if diff bank-list.txt /tmp/bank-list.txt; then
       true
       echo bank-list.txt unchanged.
@@ -15,21 +16,21 @@ if [ -d Banks ]; then
     fi
     rm /tmp/bank-list.txt
   )
-   for dir in Banks/*/history; do
-   (
-    echo "cd into $dir"
-    cd "$dir"
-    find * -prune -type d -print >/tmp/acct-list.txt
-    touch acct-list.txt
-    if diff acct-list.txt /tmp/acct-list.txt; then
-      true
-      echo "$dir/acct-list.txt unchanged."
-    else
-      cat /tmp/acct-list.txt >acct-list.txt
-      echo "$dir/acct-list.txt updated."
-    fi
-    rm /tmp/acct-list.txt
-  )
+  for dir in Banks/*/history; do
+    (
+      echo "cd into $dir"
+      cd "$dir"
+      find * -prune -type d -print >/tmp/acct-list.txt
+      [ -f "acct-list.txt" ] || touch acct-list.txt
+      if diff acct-list.txt /tmp/acct-list.txt; then
+        true
+        echo "$dir/acct-list.txt unchanged."
+      else
+        cat /tmp/acct-list.txt >acct-list.txt
+        echo "$dir/acct-list.txt updated."
+      fi
+      rm /tmp/acct-list.txt
+    )
   done
 fi
 #
